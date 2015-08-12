acc37af2373b47f256b9dca99867e80fbc17c23a
MiguelMizzi <miguelmizzi94@gmail.com>
2015-08-12 14:03:03 +0200

    AVPRO Trial
    Making use of av pro to make videos work.. success

---
+++ Assets/Scripts/Menus/PlayCutscenes.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var cutscene1 : MovieTexture;
+//var sound1 : AudioClip;
+
+function Start () {
+	
+	GetComponent.<Renderer>().material.mainTexture = cutscene1;
+	//sound1 = cutscene1.audioClip;
+	cutscene1.Play();
+	//sound1.isReadyToPlay();
+}
+
+function Update () {
+
+}
