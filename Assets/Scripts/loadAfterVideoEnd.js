41ce99af22d92028f7bbead5ac5fd4706c6cd8f4
MiguelMizzi <miguelmizzi94@gmail.com>
2015-08-20 11:29:04 +0200

    Encyclo Fully Working + AV Pro with navigation
    Encyclopedia --> Navigating through different info done, Info all
    written down, Small animation for fade in text..
    
    AVPRO scene --> Video works in scene, buttons to navigate to game level,
    after video ends, game loads next level ie game level.
    
    Encyclo --> Finished except some images + button image change
    AVPRO --> Copy this for the different cutscene levels and adjust load
    levels accordingly

---
+++ Assets/Scripts/loadAfterVideoEnd.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+	if(Application.loadedLevelName == "Cutscene1"){
+		
+		yield WaitForSeconds(49);
+		Application.LoadLevel("Level1Knights");
+	}
+}
+
+function Update () {
+
+}
+
+function loadAtEnd(){
+	
+}
