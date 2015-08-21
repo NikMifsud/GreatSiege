c031c26c4028a89449218e97491138b07a7ba8dd
MiguelMizzi <miguelmizzi94@gmail.com>
2015-08-21 11:21:46 +0200

    MainMenu1Update + Game GUI
    Starting Menu with added background + tints and working buttons
    
    Game GUI with added side bar for character details. Also with texts
    components to represent values

---
+++ Assets/Scripts/TextMapCharUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextMapCharUI : MonoBehaviour {
+
+	public Text HealthText;
+	public Text AttackText;
+	public Text CDText;
+	public Text MovementText;
+	public Text RangeText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//HealthText.text = Clicked Character Health
+	}
+}
+
